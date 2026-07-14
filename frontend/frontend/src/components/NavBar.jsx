107b78610355886e10ad0af82dc799d02694d5f6
Aymane El M'hamdi <aymanelmhamdi123@gmail.com>
2026-07-14 11:37:06 +0100

    task page changes

--- frontend/frontend/src/components/NavBar.jsx
+++ frontend/frontend/src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import { IoLogOutOutline } from "react-icons/io5";
 import { MdAdminPanelSettings } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import { FaHouseUser } from "react-icons/fa6";
+import { CiUser } from "react-icons/ci";
 
 export default function NavBar() {
   const user = JSON.parse(localStorage.getItem('user'));
@@ -14,14 +15,17 @@ export default function NavBar() {
     navigate("/login")
   }
   return (
-    <div className='flex font-serif bg-orange-100   border-b-1 pt-5 pb-5 pr-10 pl-10 '>
+    <div className={`flex font-serif    border-b-1 pt-5 pb-5 pr-10 pl-10 ${user?.role === "admin"
+      ?"bg-orange-100"
+      :"bg-green-100"
+    } `}>
       <div className='flex flex-row gap-5 justify-between w-full'>
         <div className="flex flex-row gap-5">
           <h1 className='bonjustify-between w-fullt-bold text-lg  '>Todoliste</h1>
           <p
             className={`flex border-1 border-orange-900/50  pl-2 pt-1 pr-2 rounded-lg text-[12px] text-center  bg-orange-100' ${user?.role === "admin"
-                ? "bg-orange-100 text-orange-500 border-orange-900/50"
-                : "bg-green-100 text-green-600 border-green-900/50"
+                ? "bg-white text-orange-500 border-orange-900/50"
+                : "bg-white text-green-600 border-green-900/50"
               }
           `}>
             {user?.role === "admin" ? (
@@ -37,8 +41,9 @@ export default function NavBar() {
           </p>
 
         </div>
-        <div>
-          <button className="flex hover:border-b-1 border-balck text-gray-700 hover:text-black " onClick={handlingLogout} > <IoLogOutOutline className="h-4 mr-2  mt-1 text-xl " />Sing out</button>
+        <div className="flex gap-5">
+          <p className=" flex border-1 border-gray-400  border-l-0 text-sm mt-[3px] rounded-xl pl- pr-5 text-gray-600"><CiUser className="h-6 items-center mr-3 text-sm text-white bg-green-500 rounded-xl w-7"/>{user?.username}</p>
+          <button className="flex hover:border-b-1 border-balck text-gray-900 hover:text-black " onClick={handlingLogout} > <IoLogOutOutline className="h-4 mr-2  mt-1 text-xl " />Sing out</button>
         </div>
 
       </div>
--- frontend/frontend/src/pages/user/tasks.jsx
+++ frontend/frontend/src/pages/user/tasks.jsx
@@ -2,16 +2,34 @@ import React from 'react'
 import NavBar from '../../components/NavBar'
 
 export default function tasks() {
+  const today = new Date()
+  const option = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+
+  };
+  const formattedDate=today.toLocaleDateString('en-CB',option);
+
+
   return (
-    <div>
-        <div>
-          <div>
-            <NavBar/>
 
-          </div>
-        </div>
+    <div className='flex flex-col min-h-screen'>
 
-    
+      <NavBar />
+
+
+      <div className='flex pl-[20rem] flex-col flex-1 bg-blue-100 h-full w-full p-10 '>
+        <h1 className='tracking-wide text-4xl  md:text-5xl'>Today's Liste </h1>
+        <h1 className=' flex text-lg  pt-4 ] font-medium'>{formattedDate}</h1>
+      </div>
+      <div>
+        
+      </div>
     </div>
+
+
+
   )
 }
